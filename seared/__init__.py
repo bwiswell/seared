a401aa921441cdba407de928d87b89ee103a8acc
Ben Wiswell <benjwiswell@gmail.com>
2025-04-06 02:12:45 -0400

    version/0.1.1

--- seared/__init__.py
+++ seared/__init__.py
@@ -1,6 +1,9 @@
+from .date import Date
+from ._datetime import DateTime
 from ._enum import Enum
 from .float import Float
 from .int import Int
 from .seared import Seared, seared
 from .str import Str
 from .t import T
+from .time import Time
---
+++ seared/_datetime.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+
+from marshmallow.fields import DateTime as MDateTime, Field
+
+from .field import Field, FieldMeta
+
+
+@dataclass(frozen=True)
+class DateTimeMeta(Field):
+    format: Optional[str] = None
+    missing: Optional[datetime] = None
+    
+
+@dataclass(frozen=True)
+class DateTime(DateTimeMeta, FieldMeta):
+
+    def to_field (self, _: str) -> Field:
+        return self.wrap(
+            lambda *kws: MDateTime(format=self.format, **kws),
+            data_key = self.data_key,
+            missing = self.missing
+        )
