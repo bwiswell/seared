96f8f5c4e34d6d8cd784644387d9c1cf3d58a1a4
Ben Wiswell <benjwiswell@gmail.com>
2025-04-06 07:23:20 -0400

    feat: support for booleans

---
+++ seared/bool.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from marshmallow.fields import Bool as MBool, Field
+
+from .field import Field, FieldMeta
+
+
+@dataclass(frozen=True)
+class BoolMeta(Field):
+    missing: Optional[bool] = None
+
+
+@dataclass(frozen=True)
+class Bool(BoolMeta, FieldMeta):
+
+    def to_field (self, _: str) -> Field:
+        return self.wrap(
+            MBool,
+            data_key = self.data_key,
+            load_only = not self.write,
+            missing = self.missing
+        )
